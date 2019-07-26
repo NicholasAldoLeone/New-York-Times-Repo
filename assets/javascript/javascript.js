413fb16ec2db70eefabb3c8961dfa8d30c1fc87d
shendo7 <rodneyjames@Rodneys-MacBook-Pro.local>
2019-07-26 13:52:41 -0700

    ajax call

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,8 @@
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=sobuJiPKCsf6W5ro2e1rhPnxEzcduwV5"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
