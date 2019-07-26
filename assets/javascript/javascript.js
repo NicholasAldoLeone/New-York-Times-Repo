1e3e6caf845a455667989aa2293f66d42d267e4a
shendo7 <rodneyjames@Rodneys-MacBook-Pro.local>
2019-07-26 14:05:52 -0700

    ajax call

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,12 @@
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=sobuJiPKCsf6W5ro2e1rhPnxEzcduwV5"
+$(searchButton).on("click", function (){
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=sobuJiPKCsf6W5ro2e1rhPnxEzcduwV5"
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
 
+    let article = $(this).attr("data-article")
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+    });
